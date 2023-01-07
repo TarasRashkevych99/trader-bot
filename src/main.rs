ac306f690658da4f45ba0809ce716239582b5561
AndoneSabin99 <58253647+AndoneSabin99@users.noreply.github.com>
2023-01-07 20:31:36 +0100

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use bfb;
-use rcnz_market;
-use RCNZ;
+use bfb::bfb_market::Bfb;
+use RCNZ::RCNZ;
 use ZSE::market::ZSE;
 use unitn_market_2022::good::good::Good;
 use unitn_market_2022::good::good_kind::GoodKind;
@@ -9,11 +8,16 @@ use unitn_market_2022::market::{Market, market_test, SellError};
 fn main() {
 
     //just testing if everything is ok
+
     let market_1 = RCNZ::new_random();
 
     println!("Name is {}", market_1.borrow().get_name());
 
-    let market_2 = ZSE::new_with_quantities(1.0, 1.0, 1.0, 1.0);
+    let market_2 = ZSE::new_random();
 
     println!("Name is {}", market_2.borrow().get_name());
+
+    let market_3 = Bfb::new_random();
+
+    println!("Name is {}", market_3.borrow().get_name());
 }
