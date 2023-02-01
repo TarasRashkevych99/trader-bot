2262a308f19844b5752b018529e02be6c06bb4d2
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2023-02-02 00:27:11 +0100

    add initial trade structure

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,21 @@ mod tr_traders;
 mod common;
 
 use crate::common::markets::{new_random, new_with_quantities, print_results};
+use crate::tr_traders::tr_trader_1::Trader;
 
 
 fn main() {
-    let _trader_name = "RAST".to_string();
+    let mut trader = Trader::new("RAST".to_string());
 
     // the random initialization of the markets
-    let (bfb, rcnz, zse) = new_random();
+    let (mut bfb, mut rcnz, mut zse) = new_random();
+
+    trader.trade_with_one_market(&mut bfb);
 
     print_results("Markets with random quantities", &bfb, &rcnz, &zse);
 
     // the initialization of the markets with the fixed quantity
-    let (bfb, rcnz, zse) = new_with_quantities(100.0, 100.0, 100.0, 100.0);
-
-    print_results("Markets with fixed quantities", &bfb, &rcnz, &zse);
+    // let (bfb, rcnz, zse) = new_with_quantities(100.0, 100.0, 100.0, 100.0);
+    //
+    // print_results("Markets with fixed quantities", &bfb, &rcnz, &zse);
 }
--- src/tr_traders/tr_trader_1.rs
+++ src/tr_traders/tr_trader_1.rs
@@ -1,13 +1,50 @@
+use std::borrow::BorrowMut;
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::rc::Rc;
+use unitn_market_2022::good::good::Good;
+use unitn_market_2022::good::good_kind::GoodKind;
 use unitn_market_2022::market::Market;
+
 
 type ChosenMarket = Rc<RefCell<dyn Market>>;
 
+pub struct Trader {
+    name: String,
+    wallet: HashMap<GoodKind, f32>,
+}
 
+impl Trader {
+    pub fn new(name: String) -> Self {
+        Trader {
+            name,
+            wallet: HashMap::from_iter(vec![
+                (GoodKind::EUR, 100.0),
+                (GoodKind::USD, 100.0),
+                (GoodKind::YUAN, 100.0),
+                (GoodKind::YEN, 100.0),
+            ]),
+        }
+    }
 
-pub fn trade_with(bfb: ChosenMarket, rcnz: ChosenMarket, zse: ChosenMarket) {
-    loop {
-        // trader logic
+    pub fn trade_with_one_market(&self, market: &mut ChosenMarket) {
+        // loop {
+        //     let result = market.borrow_mut().buy(&mut Good::new(GoodKind::EUR, 10.0));
+        //     match result {
+        //         Ok(token) => {
+        //             println!("Transaction successful");
+        //             market.borrow().sell(token, &mut Good::new(GoodKind::EUR, 10.0));
+        //         }
+        //         Err(e) => {
+        //             println!("Transaction failed");
+        //         }
+        //     }
+        // }
+    }
+
+    pub fn trade_with_all_markets(&self, bfb: &mut ChosenMarket, rcnz: &mut ChosenMarket, zse: &mut ChosenMarket) {
+        loop {
+            // trader logic
+        }
     }
 }
